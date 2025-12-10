f7b03fcbbabcbfe1d9bfe4357e92f85658022e38
UloakuObi <uloakuobi@yahoo.com>
2025-12-10 13:29:10 +0100

    Update styles

--- src/components/DensityBar.jsx
+++ src/components/DensityBar.jsx
@@ -2,7 +2,7 @@ export default function DensityBar({letter, count, percentage}) {
     return (
         <div className="flex items-center justify-between gap-1 density-bar">
             <p className="letter">{letter}</p>
-            <progress className="rounded-xl" max="100" value={Number(percentage)}>{percentage}</progress>
+            <progress max="100" value={Number(percentage)}>{percentage}</progress>
             <p className="count">{`${count} (${percentage}%)`}</p>
         </div>
     )
